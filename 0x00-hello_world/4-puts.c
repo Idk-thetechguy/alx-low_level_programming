f7249d050faab342d41a37c59e5a7319176edb44
Idk-thetechguy <idigweamaka@gmail.com>
2023-03-17 08:54:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
+       printf("\"Programming is like building a multilingual puzzle\n");
 
        return (0);
 }
