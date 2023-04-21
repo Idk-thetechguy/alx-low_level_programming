89a461b5a5f1b9ef709ac9d424106b510c1628b9
Idk-thetechguy <idigweamaka@gmail.com>
2023-04-21 10:19:02 -0700

    I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,17 +11,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL || cmp == NULL || size <= 0)
-		return (-1);
-
-	int i;
-
-	for (i = 0; i < size; i++)
-
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
-
-		return (-1);
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+int i;
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
 }
