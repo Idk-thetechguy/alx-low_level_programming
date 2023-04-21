9cb4464ab5398c1fc30457761a2e0964ae1bf34f
Idk-thetechguy <idigweamaka@gmail.com>
2023-04-21 12:03:46 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int get_op_func(int arg1, int arg2);
 
 /**
  * main - Entry point
@@ -12,7 +13,26 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-int (*op_func)(int, int);
+int num1;
+int num2;
+int result;
 
 if (argc != 4)
+{
+printf("Usage: %s <num1> <operator> <num2>\n", argv[0]);
+return (98);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+{
+printf("Error: Cannot divide by zero\n");
+return (100);
+}
+
+result = get_op_func(num1, num2);
+printf("%d\n", result);
+return (0);
+}
