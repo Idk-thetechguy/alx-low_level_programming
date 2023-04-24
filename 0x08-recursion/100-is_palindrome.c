0d16e1b499031e5e4732a642c8308c12351c2e10
idk-thetechguy <idigweamaka@gmail.com>
2023-04-24 06:54:43 -0700

    we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* is_palindrome - Checks if a string is a palindrome.
+* @s: Pointer to the string to be checked.
+*
+* Return: 1 if the string is a palindrome, 0 otherwise.
+*/
+int is_palindrome(char *s)
+{
+int len = 0;
+char *start, *end;
+
+if (s == NULL)
+return (0);
+
+while (*(s + len) != '\0')
+len++;
+
+start = s;
+end = s + len - 1;
+
+while (start < end)
+{
+if (*start != *end)
+return (0);
+start++;
+end--;
+}
+
+return (1);
+}
