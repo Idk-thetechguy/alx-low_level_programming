1ba143da4ec0a78cbb23bc30131bdfa90ebb2303
idk-thetechguy <idigweamaka@gmail.com>
2023-04-26 16:54:06 -0700

    we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 
 /**
-* is_palindrome - Checks if a string is a palindrome.
-* @s: Pointer to the string to be checked.
-*
-* Return: 1 if the string is a palindrome, 0 otherwise.
-*/
+ * palind2 - obtains length of a
+ * @a: string
+ * @l: integer to count length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int is_palindrome(char *s)
 {
-int len = 0;
-char *start, *end;
+	int l;
 
-if (s == NULL)
-return (0);
-
-while (*(s + len) != '\0')
-len++;
-
-start = s;
-end = s + len - 1;
-
-while (start < end)
-{
-if (*start != *end)
-return (0);
-start++;
-end--;
-}
-
-return (1);
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
