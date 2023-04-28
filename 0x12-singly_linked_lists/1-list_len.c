64e419fa2ec6de2a8a7462340144fe025e020671
idk-thetechguy <idigweamaka@gmail.com>
2023-04-27 18:22:17 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,28 +1,17 @@
 #include <stdlib.h>
+
 #include "lists.h"
 
 /**
-* list_concat - concatenates two linked lists
-* @head1: pointer to the head of the first list
-* @head2: pointer to the head of the second list
-* Return: pointer to the head of the concatenated list
+* list_len - returns the number of elements in a linked list
+* @h: pointer to the list_t list
+*
+* Return: number of elements in h
 */
-list_t *list_concat(list_t **head1, list_t **head2)
+size_t list_len(const list_t *h)
 {
-list_t *current;
-
-if (*head1 == NULL)
-return (*head2);
-
-if (*head2 == NULL)
-return (*head1);
-
-current = *head1;
-
-while (current->next != NULL)
-current = current->next;
-
-current->next = *head2;
-
-return (*head1);
+if (h == NULL)
+return (0);
+else
+return (1 + list_len(h->next));
 }
