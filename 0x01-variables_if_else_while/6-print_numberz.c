acf2fc0f503e935d4287fec264ee85b173230723
Idk-thetechguy <idigweamaka@gmail.com>
2023-05-01 12:18:26 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program that prints numbers from 0 to 9.
+*
+* Return: Always returns 0 to indicate successful execution.
+*/
+nt main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
