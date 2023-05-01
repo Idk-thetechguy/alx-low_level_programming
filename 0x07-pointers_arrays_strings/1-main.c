ae1b03d2410d24baddceed239c7448a036644119
Idk-thetechguy <idigweamaka@gmail.com>
2023-05-01 05:38:43 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -27,6 +27,23 @@ i++;
 printf("\n");
 }
 /**
+* my_memcpy - copies n bytes from memory area src to memory area dest
+* @dest: pointer to the destination array where the content is to be copied
+* @src: pointer to the source of data to be copied
+* @n: number of bytes to copy
+*
+* Return: pointer to destination.
+*/
+char *my_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
+/**
 * main - check the code
 *
 * Return: Always 0.
@@ -36,7 +53,7 @@ int main(void)
 char buffer[98] = {0};
 char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 simple_print_buffer(buffer, 98);
-_memcpy(buffer + 50, buffer2, 10);
+my_memcpy(buffer + 50, buffer2, 10);
 printf("-------------------------------------------------\n");
 simple_print_buffer(buffer, 98);
 return (0);
