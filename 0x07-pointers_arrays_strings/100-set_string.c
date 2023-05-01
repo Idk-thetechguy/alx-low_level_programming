1e06431d9a806167bcdbf6c86369160e10883930
Idk-thetechguy <idigweamaka@gmail.com>
2023-05-01 07:04:30 -0700

    My primary goal of hacking was the intellectual curiosity, the seduction of adventure

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* set_string - updates the value of a char pointer
+* @s: pointer to the char pointer
+* @to: pointer to a char
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
