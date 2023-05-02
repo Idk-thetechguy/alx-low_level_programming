e5d89fe1061d8fc92e2f374be07c73674d45a9bc
Idk-thetechguy <idigweamaka@gmail.com>
2023-05-01 17:28:41 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+
+/**
+
+ * print_sign - prints the sign of a given integer
+
+ * @n: the integer value to check
+
+ * Return: returns 1 and prints '+' if n is greater than zero
+
+ * returns 0 and prints '0' if n is equal to zero
+
+ * returns -1 and prints '-' if n is less than zero
+
+ */
+
+
