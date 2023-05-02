5009d8231a87b830e47a1308ef63c7338b3dc48d
Idk-thetechguy <idigweamaka@gmail.com>
2023-05-01 21:15:16 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,17 +3,23 @@
 
 /**
 * free_list - frees a linked list
-* @head: list_t list to be freed
+*
+* @head: pointer to the head of the list
 */
 void free_list(list_t *head)
 {
+list_t *current;
+
 if (head == NULL)
 {
 return;
 }
 
-free_list(head->next);
-
-free(head->str);
-free(head);
+while (head)
+{
+current = head;
+head = head->next;
+free(current->str);
+free(current);
+}
 }
