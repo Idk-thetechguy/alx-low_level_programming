cb068467529482b40472fddb7b108e9bf14b2ffc
Idk-thetechguy <idigweamaka@gmail.com>
2023-05-02 05:34:07 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-void before_main(void) __attribute__ ((constructor));
-
+void first(void) __attribute__ ((constructor));
 /**
-* before_main - prints a sentence before the main
-* function is executed
+* first - Preceding the primary statement, outputs a sentence
+* The function has been executed
 */
-void before_main(void)
+void first(void)
 {
-printf("Hear me, oh ye of little faith. Do not despair, for I am here!\n\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
