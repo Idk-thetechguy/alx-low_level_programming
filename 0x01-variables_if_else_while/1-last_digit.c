819ae94e7022ac97fc8c3185df61127ece0a678e
Idk-thetechguy <idigweamaka@gmail.com>
2023-05-05 14:24:45 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,17 +14,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("Last digit of 98 is 8 and is greater than 5\n");
+printf("Last digitvof n is %d and is greater than 5\n", n);
 }
 else
 if (n == 0)
 {
-printf("Last digit of -98 is -8 and is 0\n");
+printf("Last digit of n is %d and is 0\n", n);
 }
 else
 if (n <= 0)
 {
-printf("Last digit of 980 is 0 and is less than 6 and not 0\n");
+printf("Last digit of n is %d and is greater than 6 and not 0\n", n);
 }
 return (0);
 }
