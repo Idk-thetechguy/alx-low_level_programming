524e6ba83c7ae970050ea2573bd9af17634bd5e0
Idk-thetechguy <119006247+Idk-thetechguy@users.noreply.github.com>
2023-06-07 00:53:03 +0100

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Compile all .c files with strict warnings
-gcc -Wall -Wextra -Werror -pedantic -c *.c
-# Create a static library with all object files
-ar rcs liball.a *.o
-# Index the static library
-ranlib liball.a
