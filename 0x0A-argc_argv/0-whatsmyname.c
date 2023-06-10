4931ec4eb8fc154e006c15e37858c9c3e7004f9a
Idk-thetechguy <idigweamaka@gmail.com>
2023-06-09 20:39:25 -0700

    Answer

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point of the program
+* @argc: The number of command-line arguments passed to the program
+* @argv: An array of strings containing the command-line arguments
+*
+* Return: 0 on successful execution
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
