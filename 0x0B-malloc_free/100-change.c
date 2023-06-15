e744c62cc46c8a0b97292c6c821d18d4a2ac60e4
Idk-thetechguy <idigweamaka@gmail.com>
2023-06-14 21:47:44 -0700

    Minimal Number of Coins for Change

---
+++ 0x0B-malloc_free/100-change.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Displays the sum of non-negative integers.
+*        followed by a new line.
+* @argc: The quantity of parameters supplied to the program.
+* @argv: A collection of references to the parameters.
+*
+* Return: If any of the numbers includes non-digit symbols - 1.
+*         Otherwise - 0.
+*/
+int main(int argc, char *argv[])
+{
+int num, digit, sum = 0;
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
+return (0);
+}
